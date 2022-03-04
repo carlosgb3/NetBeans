dce6e7f45783e5c96a7743d032b254f04ea4a4b2
“carlosgb3000@gmail.com” <carlosgb3000@gmail.com>
2022-03-04 15:33:07 +0100

    “version2.0”

---
+++ proyecto_git/src/main/java/contornos/proyecto_git/Restador.java
@@ -0,0 +1,8 @@
+package contornos.proyecto_git;
+
+public class Restador {
+
+    public static float restar(float a, float b) {
+        return a - b;
+    }
+}
--- proyecto_git/src/main/java/contornos/proyecto_git/Sumador.java
+++
@@ -1,8 +0,0 @@
-package contornos.proyecto_git;
-
-public class Sumador {
-
-    public static float sumar(float a, float b) {
-        return a + b;
-    }
-}
